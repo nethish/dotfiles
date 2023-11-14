0e6c37d07245fa5ac10f4cdb6fd2b51cac6b51b5
nethish <nethish259@gmail.com>
2023-11-14 20:12:58 +0530

    Enable vim in command line baby :bomb:

--- functions.sh
+++ functions.sh
@@ -2,6 +2,19 @@ _mkcd () {
 	mkdir -p $@ && z ${@:$#}
 }
 
+function get_jobs_cnt() {
+  local JOBS_CNT=''
+  JOBS_CNT+="$(jobs -s | wc -l)"
+  echo "$JOBS_CNT"
+}
+
+function get_custom_exit_code() {
+  local LAST_EXIT=$?
+  local LOCAL_RPROMPT=''
+  LOCAL_RPROMPT+=%{$fg[red]%}-$LAST_EXIT-%{$reset_color%}
+  echo "$LOCAL_RPROMPT"
+}
+
 if ! typeset -f mkcd > /dev/null; then
     alias mkcd="_mkcd"
 fi

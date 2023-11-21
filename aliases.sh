d4b8d7249cc3193190e7e7f7306ccf0d903b7e75
nethish <nethish259@gmail.com>
2023-11-21 22:41:00 +0530

    New aliases

--- aliases.sh
+++ aliases.sh
@@ -1,4 +1,5 @@
 # heheh
+alias v="nvim"
 alias vim="nvim"
 
 alias lg=lazygit
@@ -11,3 +12,28 @@ alias sb="source ~/.bashrc"
 alias sl="source ~/.localrc"
 alias sl="source ~/.aliases"
 alias -g zz=" | fzf"
+alias p="python3"
+alias ds="du -sh"
+alias rm='rm -i'
+
+
+alias tmp="mkcd ~/repos/tmp"
+
+# GIT
+alias g="git"
+alias ga="git add"
+alias gm="git commit -m"
+alias gd="git diff"
+alias gs="git status"
+alias gr="git restore"
+alias grs="git restore --staged"
+alias grev="git revert"
+alias gstl="git stash list"
+alias gsta="git stash apply"
+alias gstp="git stash pop"
+alias gsts="git stash"
+alias gb="git branch"
+alias gco="git checkout"
+alias gcb="git checkout -b"
+alias gr="git remote"
+alias gp="git push"

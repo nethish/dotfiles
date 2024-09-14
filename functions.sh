8bc20ac87e7b1e15971f5dd889d237b98915afa3
nethish <nethish259@gmail.com>
2024-09-14 11:47:35 +0530

    add alias shortcut

--- functions.sh
+++ functions.sh
@@ -18,3 +18,12 @@ function get_custom_exit_code() {
 if ! typeset -f mkcd > /dev/null; then
     alias mkcd="_mkcd"
 fi
+
+# Aliases `a` to the last execute command. Pass an argument to alias to that
+# Example
+# $ al
+# $ al b
+al() {
+  local alias_name=${1:-a}
+  alias $alias_name="$(fc -ln -1)"
+}
